66cc2c7e9b98244da09610823d954f1015db9459
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-20 16:14:54 -0500

    Experimenting RestSharp

---
+++ APIRequest/APIRequest/Program.cs
@@ -0,0 +1,29 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            //client.BaseUrl = new Uri("http://twitter.com");
+            //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+            var request = new RestRequest("/users", Method.GET);
+            //request.Resource = "statuses/friends_timeline.xml";
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            Console.WriteLine("Response.Content: " + content);
+                
+
+        }
+    }
+}
