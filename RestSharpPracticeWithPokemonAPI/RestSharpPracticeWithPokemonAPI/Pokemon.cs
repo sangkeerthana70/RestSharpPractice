0a7bfb9c036a58ec1f6ec9e8d9c1d3368e74c232
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-06 16:19:56 -0500

    Create a class file for Pokemon

--- RestSharpPracticeWithPokemonAPI/RestSharpPracticeWithPokemonAPI/Pokemon.cs
+++ RestSharpPracticeWithPokemonAPI/RestSharpPracticeWithPokemonAPI/Pokemon.cs
@@ -8,16 +8,48 @@ namespace RestSharpPracticeWithPokemonAPI
 {
     public class Pokemon
     {
-        public int PokeId { get; set; }
+        public string PokeId { get; set; }
         public string Name { get; set; }
         public string SuperType { get; set; }
         public string SubType { get; set; }
         public string artist { get; set; }
-        public string rarity { get; set; }
-        public string AttacksCost { get; set; }
-        public string AttacksName { get; set; }
-        public string AttacksText { get; set; }
-        public string Weaknesses { get; set; }
+        public string rarity { get; set; }        
+        public string Attack1Name { get; set; }
+        public string Attack1Text { get; set; }
+        public string Attack2Name { get; set; }
+        public string Attack2Text { get; set; }
+        public string WeaknessType { get; set; }
 
+        public Pokemon()
+        {
+            Name = "";
+            SuperType = "";
+            SubType = "";
+            artist = "";
+            rarity = "";
+            Attack1Name = "";
+            Attack1Text = "";
+            Attack2Name = "";
+            Attack2Text = "";
+            WeaknessType = "";
+
+        }
+
+        public void About()
+        {
+            Console.WriteLine("My Pokemon Id : " + this.PokeId);
+            Console.WriteLine("My name is: " + Name);
+            Console.WriteLine("I am a " + SuperType + SubType + "type");
+            Console.WriteLine("My artist is: " + artist);
+            Console.WriteLine("My rarity is: " + rarity);
+            Console.WriteLine("My attackName is : " + Attack1Name);
+            Console.WriteLine("My attack description : " + Attack1Text);
+            Console.WriteLine("My attackName is : " + Attack2Name);
+            Console.WriteLine("My attack description : " + Attack2Text);
+            Console.WriteLine("My Weakness : " + WeaknessType);
+        }
     }
+    
+
 }
+
